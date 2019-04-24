34d4a0326d96bd210c8c86381707d1176a5fdaaa
James Long <jlongc12@gmail.com>
2019-04-24 10:58:15 -0400

    update shell

--- shell/run_sim.sh
+++ shell/run_sim.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --partition=ultrasound
-#SBATCH --mem=16000
+#SBATCH --mem=8000
 #SBATCH --cpus-per-task=8
-#SBATCH --array=1-5000%50
+#SBATCH --array=1-10000%100
 
 date
 hostname
 
 module load Matlab/R2017b
-matlab -nojvm -nodisplay -nosplash -r "cd('/datacommons/ultrasound/jc500/GIT/imagenet_ultrasound/');tic;sim_img($SLURM_ARRAY_TASK_ID,50);toc;exit;"
+matlab -nojvm -nodisplay -nosplash -r "cd('/datacommons/ultrasound/jc500/GIT/imagenet_ultrasound/simulation/');tic;sim_img($SLURM_ARRAY_TASK_ID,20);toc;exit;"
--- shell/run_stitch.sh
+++ shell/run_stitch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #SBATCH --partition=ultrasound
-#SBATCH --mem=32000
-#SBATCH --cpus-per-task=4
-#SBATCH --array=11,96%2
+#SBATCH --mem=8000
+#SBATCH --cpus-per-task=8
+#SBATCH --array=1-500%100
 
 date
 hostname
 
 module load Matlab/R2017b
-matlab -nojvm -nodisplay -nosplash -r "cd('/datacommons/ultrasound/jc500/GIT/imagenet_ultrasound/');tic;stitch_img($SLURM_ARRAY_TASK_ID,50);toc;exit;"
+matlab -nojvm -nodisplay -nosplash -r "cd('/datacommons/ultrasound/jc500/GIT/imagenet_ultrasound/simulation/');tic;stitch_img($SLURM_ARRAY_TASK_ID,20);toc;exit;"
